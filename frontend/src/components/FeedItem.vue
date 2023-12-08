8c57326a1304fa669c8be4426204e7f79679125f
Sylviaviaviavia <103002698+Sylviaviaviavia@users.noreply.github.com>
2023-12-07 23:21:46 -0800

    update

--- frontend/src/components/FeedItem.vue
+++ frontend/src/components/FeedItem.vue
@@ -18,6 +18,7 @@
 
         <div class="my-6 flex justify-between">
             <div class="flex space-x-6 items-center">
+                <!--
                 <div class="flex items-center space-x-2" @click="likePost(post.post_id)">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"></path>
@@ -25,6 +26,7 @@
                     
                     <span class="text-gray-500 text-xs">{{ post.likes_count }} likes</span>
                 </div>
+                -->
 
                 <div v-if="post.is_private" class="flex items-center space-x-2 text-gray-500 text-xs">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
